764f8ad09aa0fc52e603ad04f14138eed8c062db
Manish Saini <46782688+manishsaini8@users.noreply.github.com>
2022-12-08 18:05:39 +0530

    100 -3

---
+++ Point4Purpose/app/etc/env.php
@@ -0,0 +1,87 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'dadmin'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'f977e8d05198273fef255312fbf9998d'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'userdbs_m2p4pnew_dev',
+                'username' => 'root',
+                'password' => 'kr1pt0405724976',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'production',
+    'session' => [
+        'save' => 'db'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '8bc_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '8bc_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 0,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1,
+        'amasty_shopby' => 1
+    ],
+    'downloadable_domains' => [
+        'points4purpose.com.au'
+    ],
+    'install' => [
+        'date' => 'Sat, 05 Jun 2021 08:14:20 +0000'
+    ]
+];
